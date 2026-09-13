57daf7f51c12817c1b24740e3937e533e0e3ecd3
Ruediger Mueck <ruediger.mueck@gmail.com>
2026-09-13 12:10:28 +0200

    Working greyish

--- script.js
+++ script.js
@@ -8,10 +8,9 @@ const audioMotion = new AudioMotionAnalyzer(
   {
     source: audioEl,
     height: window.innerHeight - 50,
-    mode: 3,
     radial: 1,
-    showPeaks: 1,
     gradient: "prism",
+    mode: 3,
     barSpace: 0.6,
     ledBars: true
   }
@@ -21,7 +20,8 @@ document.getElementById("version").textContent =
   `v${AudioMotionAnalyzer.version}`;
 
 document.getElementById("live").addEventListener("click", () => {
-  audioEl.src = "https://babaganousha.net:8443/radio.mp3";
+  // audioEl.src = "https://icecast2.ufpel.edu.br/live";
+  audioEl.src = "https://babaganousha.net:8443/;";
   audioEl.play();
 });
 
